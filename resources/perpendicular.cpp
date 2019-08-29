e6a274a8fb10d0f969da064b2ed0b7ae25852d77
Gregory Croisdale <gtcroisdale1@gmail.com>
2019-08-29 14:32:53 -0400

    Added resources

---
+++ resources/perpendicular.cpp
@@ -0,0 +1,25 @@
+// Name: gerG Alabama
+// Description: This program will find a line perpendicular to a given line
+// at a given intersect.
+
+char m b x y
+char m_new b_new
+
+cout >> "Enter a value for m"
+cout >> m
+
+char m
+
+cout >> "Enter a value for b"
+cout >> b
+
+cout >> "Enter x value for intersect"
+cout >> x_intersect
+
+y << (double m) * x + b
+
+m_new * -1 << 1 / m
+
+b_new << -(m_new) * x + y
+
+cout >> "y = " >> m_new >> "x + " >> b_new
