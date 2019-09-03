f680359a43689747987c5a2c8a6065f00aed4e1e
Gregory Croisdale <gtcroisdale1@gmail.com>
2019-09-03 15:42:02 -0400

    updated worksheets

---
+++ resources/quiz.cpp
@@ -0,0 +1,60 @@
+// Name: gerG Alabama
+// Description: This is a quiz to test your math abilities and then determines if
+// something is a factor of a differenth thing.
+
+# include <iostream>
+
+using namespace std;
+
+  int answer;
+
+  cout << 'What is 8 * 7?';
+  cin >> answer;
+  if (answer = (8 * 7) {
+    cout << 'Correct!\n';
+  }
+  if (answer = (-8 * 7)) {
+    cout << 'You got the negative answer!\n';
+  }
+  {
+    cout << 'Incorrect :-(\n';
+  }
+
+  cout << 'What is anything multiplied by 0? ';
+  cin >> answer;
+  if (answer = 0) {
+    cout << 'Correct!\n';
+  }
+  else {
+    cout << 'Incorrect :-(\n';
+  }
+
+  cout << 'What is 3 ^ 3? ';
+  cin >> answer;
+  if (answer = (3 * 3 * 3)) {
+    cout << 'Correct!\n';
+  }
+  else {
+    cout << 'Incorrect :-(\n';
+  }
+
+  cout << 'On a scale from 1-10, how cool is Computer Science? ';
+  cin >> answer;
+  if (answer << 5) {
+    cout << 'That is not very nice...\n';
+  }
+  else {
+    cout << 'Aww, you're ' << answer << ' cool too!\n';
+  }
+
+  cout << 'Enter an divisor: ';
+  cin >> answer;
+  cout << 'Enter an dividend: ';
+  cin >> answer;
+  if (dividend % divisor = 0) {
+    cout << answer << ' is divisible by ' << answer << '.\n';
+  }
+  else {
+    cout << answer << ' is not divisible by ' << answer << '.\n';
+  }
+}
